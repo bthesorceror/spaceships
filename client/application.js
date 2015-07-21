98c695b94ada2dd56b4b9f8eee450a2e7f12b332
bthesorceror <bthesorceror@gmail.com>
2015-07-20 22:21:17 -0600

    Add toggle for full screen

--- client/application.js
+++ client/application.js
@@ -1,8 +1,50 @@
 var domready = require('domready');
 
+var inherits     = require('util').inherits;
+var EventEmitter = require('events').EventEmitter;
+
+inherits(FullScreenToggle, EventEmitter);
+
+function FullScreenToggle() {
+  EventEmitter.call(this);
+}
+
+FullScreenToggle.prototype.isFullScreen = function() {
+  return document.webkitFullscreenElement;
+}
+
+FullScreenToggle.prototype.toggle = function() {
+  if (this.isFullScreen()) {
+    document.webkitExitFullscreen();
+    this.emit('off');
+  } else {
+    document.documentElement.webkitRequestFullscreen();
+    this.emit('on');
+  }
+}
+
+function setupToggle() {
+  var button = document.querySelector('#fullscreenToggle');
+  var toggle = new FullScreenToggle();
+
+  toggle.on('on', function() {
+    button.classList.add('exitFullscreen');
+    button.classList.remove('enterFullscreen');
+  });
+
+  toggle.on('off', function() {
+    button.classList.add('enterFullscreen');
+    button.classList.remove('exitFullscreen');
+  });
+
+  button.addEventListener('mouseup', toggle.toggle.bind(toggle));
+}
+
 domready(function() {
   var canvas  = document.querySelector('#gameScreen');
   var context = canvas.getContext('2d');
+
+  setupToggle();
 
   function draw() {
     var centerX = window.innerWidth / 2;
@@ -20,15 +62,18 @@ domready(function() {
     context.restore();
   }
 
-  function resize() {
+  function setDimensions() {
     canvas.setAttribute('width', window.innerWidth);
     canvas.setAttribute('height', window.innerHeight);
+  }
+
+  function onResize() {
+    setDimensions();
     draw();
   }
 
-  window.addEventListener('resize', function(e) {
-    resize();
-  });
+  window.addEventListener('resize', onResize);
 
-  resize();
+  setDimensions();
+  draw();
 });
