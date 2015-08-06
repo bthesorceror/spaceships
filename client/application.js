6cca8bdc76178e089f0dc160e2777e6d1e3b86d4
bthesorceror <bthesorceror@gmail.com>
2015-08-06 02:04:59 -0600

    Simple mini map

--- client/application.js
+++ client/application.js
@@ -64,6 +64,71 @@ domready(function() {
     screen.setCenteredOn(x, y);
   }
 
+  var miniMapCanvas = document.querySelector('#miniMap');
+  var miniMapContext = miniMapCanvas.getContext('2d');
+
+  function setMiniMapSize() {
+    var percent = .25;
+    miniMapCanvas.width = screen.getWidth() * percent;
+    miniMapCanvas.height = screen.getWidth() * percent;
+  }
+  setMiniMapSize();
+
+  function clearMiniMap() {
+    miniMapContext.clearRect(0, 0,
+      miniMapCanvas.width, miniMapCanvas.height);
+
+    miniMapContext.save();
+    miniMapContext.fillStyle="rgba(0,0,0,0.9)";
+    miniMapContext.fillRect(0, 0,
+      miniMapCanvas.width, miniMapCanvas.height);
+    miniMapContext.restore();
+  }
+
+  function drawScreenOnMiniMap(screen) {
+    var adjustmentX = (miniMapCanvas.width / MAXWIDTH);
+    var adjustmentY = (miniMapCanvas.height / MAXHEIGHT);
+
+    var x = adjustmentX * screen.centeredOn.x;
+    var y = adjustmentY * screen.centeredOn.y;
+
+    var width = adjustmentX * screen.getWidth() / 2;
+    var height = adjustmentX * screen.getHeight() / 2;
+
+    miniMapContext.save();
+    miniMapContext.strokeStyle = '#00F';
+    miniMapContext.translate(x, y);
+
+    miniMapContext.beginPath();
+    miniMapContext.moveTo(-width,-height);
+    miniMapContext.lineTo(width, -height);
+    miniMapContext.lineTo(width, height);
+    miniMapContext.lineTo(-width, height);
+    miniMapContext.closePath();
+
+    miniMapContext.stroke();
+    miniMapContext.restore();
+  }
+
+  function drawOnMiniMap(obj, color) {
+    var x = (miniMapCanvas.width / MAXWIDTH) * obj.position.x;
+    var y = (miniMapCanvas.height / MAXHEIGHT) * obj.position.y;
+
+    miniMapContext.save();
+    miniMapContext.fillStyle = color;
+    miniMapContext.translate(x, y);
+
+    miniMapContext.beginPath();
+    miniMapContext.moveTo(-1,-1);
+    miniMapContext.lineTo(1, -1);
+    miniMapContext.lineTo(1, 1);
+    miniMapContext.lineTo(-1, 1);
+    miniMapContext.closePath();
+
+    miniMapContext.fill();
+    miniMapContext.restore();
+  }
+
   ship.on('positionChanged', function(position) {
     focusOnShip();
   });
@@ -71,8 +136,13 @@ domready(function() {
   setupToggle();
 
   function draw() {
+    drawScreenOnMiniMap(screen);
+    drawOnMiniMap(ship, '#0F0');
     ship.draw(screen);
-    rocks.forEach(function(r) { r.draw(screen) });
+    rocks.forEach(function(r) {
+      r.draw(screen);
+      drawOnMiniMap(r, r.color);
+    });
   }
 
   function update() {
@@ -82,6 +152,7 @@ domready(function() {
 
   function setDimensions() {
     screen.setDimensions(window.innerWidth, window.innerHeight);
+    setMiniMapSize();
   }
 
   function checkCollisions() {
@@ -95,6 +166,7 @@ domready(function() {
   function loop() {
     window.requestAnimationFrame(loop)
     screen.clear();
+    clearMiniMap();
     update();
     checkCollisions();
     draw();
